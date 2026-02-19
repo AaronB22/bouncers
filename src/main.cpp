d306acaeb3900463e362bb29feed57e8b75e4e71
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-02-19 15:40:00 +0000

    Add comments

--- src/main.cpp
+++ src/main.cpp
@@ -9,55 +9,73 @@
 
 #include "bn_sprite_items_dot.h"
 
+// Set max/min x position to be the edges of the display
 static constexpr int HALF_SCREEN_WIDTH = bn::display::width() / 2;
-
 static constexpr bn::fixed MIN_X = -HALF_SCREEN_WIDTH;
 static constexpr bn::fixed MAX_X = HALF_SCREEN_WIDTH;
 
+// Starting speed of a bouncer
 static constexpr bn::fixed BASE_SPEED = 2;
 
+// Maximum number of bouncers on screen at once
 static constexpr int MAX_BOUNCERS = 20;
 
 
 int main() {
     bn::core::init();
 
+    // Sprites and speeds of bouncers
+    // Items with the same index correspond to each other
     bn::vector<bn::sprite_ptr, MAX_BOUNCERS> sprites = {};
     bn::vector<bn::fixed, MAX_BOUNCERS> speeds = {};
 
     while(true) {
-
+        // if A is pressed add a new bouncer
         if(bn::keypad::a_pressed()) {
+            // Only add if we're below the maximum
             if(sprites.size() < sprites.max_size()) {
                 sprites.push_back(bn::sprite_items::dot.create_sprite());
                 speeds.push_back(BASE_SPEED);
             }
         }
 
+        // if B is pressed print the average to the console
         if(bn::keypad::b_pressed()) {
+            // Add all x positions together
             bn::fixed x_sum = 0;
             for(bn::sprite_ptr sprite : sprites) {
                 x_sum += sprite.x();
             }
+
             bn::fixed x_average= x_sum;
+
+            // Only divide if we have 1 or more
+            // Prevents division by 0
             if(sprites.size() > 0) {
                 x_average /= sprites.size();
             }
+
             BN_LOG("Average x: ", x_average);
         }
 
+        // for each bouncer
         for(int i = 0; i < sprites.size(); i++) {
             bn::sprite_ptr sprite = sprites[i];
 
             bn::fixed x = sprite.x();
 
+            // Update x position by adding speed
             x += speeds[i];
 
+            // If we've gone off the screen on the right
             if(x > MAX_X) {
+                // Snap back to screen and reverse direction
                 x = MAX_X;
                 speeds[i] *=-1;
             }
+            // If we've gone off the screen on the left
             if(x < MIN_X) {
+                // Snap back to screen and reverse direction
                 x = MIN_X;
                 speeds[i] *= -1;
             }
